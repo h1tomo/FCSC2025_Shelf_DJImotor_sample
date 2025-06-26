4feabbd9f9dbddb61032e181ef372f4318b675fa
h1tomo <h1.tomohero@gmail.com>
2025-06-27 08:47:47 +0900

    シリアル周りのサンプルコードを追加した

--- src/main.cpp
+++ src/main.cpp
@@ -28,10 +28,23 @@ const double gear = 36.1;
 const double mm_to_pulse_Pos = 2/d2 *d2/d1*gear *8192/PI2;
 const double mm_to_pulse_Mov = 2/d3 *d2/d1 *gear *8192/PI2;
 
-//変数及び制御パラメータ
+//制御モータ制御関連パラメータ
 bool moving = false;
 float trgPos[] = {0,0,0,0};
 float speed[] = {200, 100, 200, 100};
+
+//UART通信関係変数
+bool rcv = false;
+bool findHeader = false;
+int16_t rcvData[4];
+uint8_t rcvBinary[3];
+long sendTime = 0;
+int cnt = 0;
+
+//送信データ配列
+uint8_t sendData[10];
+
+//作業完了状態配列
 
 
 //--------------------------------------------------------
@@ -47,7 +60,7 @@ inline int toRealData(unsigned char DataH, unsigned char DataL){
 void can_send(){
     canMsgOut.can_id = 0x200;
     canMsgOut.can_dlc = 8;
-    for (size_t i = 0; i < NoM/2; i++){
+    for (size_t i = 0; i < NoM; i++){
         //if(i> 3) break;
         canMsgOut.data[i*2]   = (char)(set_current[i] / 256); // High 8 bit
         canMsgOut.data[i*2+1] = (char)(set_current[i] % 256); // Low  8 bit
@@ -84,18 +97,18 @@ void drawLCD(){
 //一定速度でモーター1個動作させるための関数
 void moveShelfs(float trgPos_[], float speed[], int ID_){
   float dif = trgPos_[ID_] - motor[ID_] -> get_pos();
-  Serial.printf("trgPos = %5.1f, crtPos = %5.1f, dif = %5.1f \n", trgPos_[ID_], motor[ID_] -> get_pos(), dif);
+  //Serial.printf("trgPos = %5.1f, crtPos = %5.1f, dif = %5.1f \n", trgPos_[ID_], motor[ID_] -> get_pos(), dif);
   float step_size = 1.0;//mm
   float samples = fabs(dif / step_size);
   if(samples < 10)samples =10;
-  Serial.printf("samples = %5.1f", samples);
+  //Serial.printf("samples = %5.1f", samples);
   int period_ms = 1000* step_size/speed[ID_];
   unsigned long myTimer = millis();
   float tg_pos;
   float startPos = motor[ID_] ->get_pos();
   for (size_t i = 1; i <= samples; i++){
     tg_pos = startPos + (trgPos_[ID_] - startPos)* i/samples;
-    printf("i = %4d, tg_pos = %4.1f\n", i, tg_pos);
+    //printf("i = %4d, tg_pos = %4.1f\n", i, tg_pos);
     motor[ID_]  -> set_trg(gm_max_cur*0.3, tg_pos);
     myTimer += period_ms;
     //Serial.printf("%5.1f, %5.1f\n", tg_posX, tg_posY);
@@ -143,7 +156,13 @@ void Core1b(void *args) {
     if(moving){
       moveShelfs(trgPos, speed, 0);
       moveShelfs(trgPos, speed, 1);
+      moveShelfs(trgPos, speed, 2);
+      moveShelfs(trgPos, speed, 3);
       moving = false;
+      motor[1] -> set_pos(0);
+      motor[1] -> set_trg(0,0);
+      motor[3] -> set_pos(0);
+      motor[3] -> set_trg(0,0);
     }
   }
 }
@@ -157,6 +176,7 @@ void setup() {
   M5.begin(true,false,true,false);
   delay(50);
 
+  //UART通信のセットアップ
   Serial.begin(115200);
 
   //can init
@@ -174,18 +194,18 @@ void setup() {
 
   //モーターのPIDゲインと変換パラメータ設定してインスタンス生成
   for (size_t i = 0; i < NoM/2; i++){
-    Serial.println(i);
+    //Serial.println(i);
     motor[i*2]   = new DJI_mController(mm_to_pulse_Pos, 0.1, 0, 0.1); //mm_to_pulse_Pos
     motor[i*2+1] = new DJI_mController(mm_to_pulse_Mov, 0.1, 0, 0.1); 
   }
 
-  Serial.print("can connecting");
+  //Serial.print("can connecting");
   while(1){//canが接続するまでループ
     if (mcp2515.readMessage(&canMsg) == MCP2515::ERROR_OK ) break;
     if(M5.BtnB.read()==HIGH)break;
-    Serial.print(".");
+    //Serial.print(".");
   }
-  Serial.println("CAN is OK");
+  //Serial.println("CAN is OK");
 
   //マルチタスク設定
   xTaskCreateUniversal(Core0a,"Core0a",8192,NULL,1,NULL,PRO_CPU_NUM);
@@ -216,7 +236,10 @@ void setup() {
 void loop() {
   //ボタンAが押されたとき
   if(M5.BtnA.read() == 1){
+    trgPos[0] = 300;
     trgPos[1] = 150;
+    trgPos[2] = 300;
+    trgPos[3] = 150;
     moving = true;
     while(M5.BtnA.read()){
       delay(10);
@@ -235,18 +258,102 @@ void loop() {
   }
   //ボタンCが押されたとき
   if(M5.BtnC.read() == 1){
-    trgPos[0] = 300;
+    trgPos[0] = 0;
+    trgPos[1] = -150;
+    trgPos[2] = 0;
+    trgPos[3] = -150;
     moving = true;
     while(M5.BtnC.read()){
       delay(10);
     }
   }
 
-  //動作完了判定
-  if(moveCmp(trgPos)){
-    Serial.println(">>[INFO] move completed!!");
+  //シリアル受信処理
+  // ヘッダー検出
+  while (!findHeader && Serial.available() > 0) {
+    if (Serial.read() == 0xFF) {
+      findHeader = true;
+    }
+  }
+  // ヘッダー検出後、必要なバイト数があるか確認
+  if (findHeader && Serial.available() >= 12) {
+    for (int i = 0; i < 4; i++) {
+      // 3バイト受信
+      for (int j = 0; j < 3; j++) {
+        rcvBinary[j] = Serial.read();
+      }
+      // デコード処理（変更しない）
+      uint16_t high = ((rcvBinary[0] << 6) | (rcvBinary[1] >> 1)) & 0xFF;
+      uint16_t low  = ((rcvBinary[1] << 7) | rcvBinary[2]) & 0xFF;
+      rcvData[i] = (high << 8) | low;
+    }
+    rcv = true;
+    findHeader = false;
+    cnt = 0;
+    // 表示更新
+    M5.Lcd.fillRect(0, 80, 320, 20, BLACK);  // 黒でクリア
+    while (Serial.available() > 0) {
+      Serial.read();// 残っているバイトを読み捨てる
+    }
+
+    // モーター目標値登録や表示灯の設定など
+    int id = rcvData[0];
+    trgPos[id*2] = rcvData[1];
+    trgPos[id*2+1] = rcvData[2];
+    moving = true;
   }
 
-  Serial.printf("%3.1f   %3.1f   %3.1f   %3.1f\n", motor[0] -> get_pos(), motor[1] -> get_pos(), motor[2] -> get_pos(), motor[3] -> get_pos());
+  //作業状態配列を更新
+
+  //シリアル送信処理
+  if(millis() - sendTime > 1000){
+    //各棚の状態と非常停止押下状態の送信
+    uint16_t sendData[10];
+    if(rcv && cnt < 2){
+      sendData[0] = 255;
+      sendData[1] = 1;
+      sendData[2] = 1;
+      sendData[3] = 1;
+      sendData[4] = 1;
+      sendData[5] = 1;
+      sendData[6] = 1;
+      sendData[7] = 0;
+      sendData[8] = 0;
+      sendData[9] = 1;
+      cnt++;
+    }
+    else if(rcv && cnt >= 2 && cnt < 4){
+      sendData[0] = 255;
+      sendData[1] = 1;
+      sendData[2] = 1;
+      sendData[3] = 1;
+      sendData[4] = 1;
+      sendData[5] = 1;
+      sendData[6] = 1;
+      sendData[7] = 0;
+      sendData[8] = 0;
+      sendData[9] = 0;
+      cnt++;
+    }
+    else{
+      sendData[0] = 255;
+      sendData[1] = 0;
+      sendData[2] = 0;
+      sendData[3] = 0;
+      sendData[4] = 0;
+      sendData[5] = 0;
+      sendData[6] = 0;
+      sendData[7] = 0;
+      sendData[8] = 0;
+      sendData[9] = 0;
+    }
+    
+    for(int i = 0; i < 10; i++){
+      Serial.write(sendData[i]);
+    }
+    sendTime = millis();
+  }
+
+  //Serial.printf("%3.1f   %3.1f   %3.1f   %3.1f\n", motor[0] -> get_pos(), motor[1] -> get_pos(), motor[2] -> get_pos(), motor[3] -> get_pos());
   delay(50);
 }
